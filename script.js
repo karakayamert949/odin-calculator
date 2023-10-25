c2b041b6400a452f44d009a41926382c2cc655e2
karakayamert949 <karakayamert949@gmail.com>
2023-10-25 14:01:42 +0300

    Now user can input floating numbers

--- script.js
+++ script.js
@@ -14,22 +14,24 @@ const divide = function(x,y){
     return x/y
 }
 
-// num1 num2 should be string, operate should return string
+// num1 num2 should be number, operate should return string
 const operate = function(num1,num2,operator){
     if (operator==='/'){
-        return divide(num1,num2).toString()
+        if (num2==0)
+            return "Error: cannot divide a number by zero"
+        else
+            return divide(num1,num2).toFixed(3)
     }
     else if (operator==='x'){
-        return multiply(num1,num2).toString()
+        return multiply(num1,num2).toFixed(3)
     }
     else if (operator==='+'){
-        return add(num1,num2).toString()
+        return add(num1,num2).toFixed(3)
     }
     else if (operator==='-'){
-        return subtract(num1,num2).toString()
+        return subtract(num1,num2).toFixed(3)
     }
 }
-
 const numBtns = document.querySelectorAll('.numBtn');
 const operatorBtns = document.querySelectorAll('.operatorBtn');
 const dispScreen = document.querySelector('.displayScreen');
@@ -51,42 +53,53 @@ numBtns.forEach((numBtn) =>{
 operatorBtns.forEach((optBtn) => {
     optBtn.addEventListener('click',
     () => {
-        let optOld = opt;
-        opt = optBtn.textContent;
-        
-        if (opt ==='Clear'){
-            disp = "";
-            num1= undefined;
-            num2=undefined;
-            opt=undefined;
+        if (optBtn.textContent ==='.'){
+            if (disp.slice(-1)==' ') return
+            else if (num1 !==undefined){
+                if (disp.slice(num1.length+3).includes('.')) return
+            }
+            else if (num2 === undefined && disp.includes('.')) return
+            
+            disp=disp.concat('.');
             dispScreen.textContent = disp;
         }
-        else if (disp === "") return
-        else if (isNaN(disp.slice(-1)) || disp.slice(-1)===' ') return
-        else if ((opt ==='=' && optOld===undefined) || (optOld==='=' && opt==='=')) return
-        else if (opt ==='='){
-            idx = disp.lastIndexOf(optOld);
-            num2 = disp.slice(idx+2);
-            disp = disp.concat(' = ', operate(Number(num1),Number(num2),optOld));
-            dispScreen.textContent = disp;
-            disp = "";
-            num1= undefined;
-            num2=undefined;
-            opt=undefined;
+        else{
+            let optOld = opt;
+            opt = optBtn.textContent;
+            
+            if (opt ==='Clear'){
+                disp = "";
+                num1= undefined;
+                num2=undefined;
+                opt=undefined;
+                dispScreen.textContent = disp;
+            }
+            else if (disp === "") return
+            else if (isNaN(disp.slice(-1)) || disp.slice(-1)===' ') return
+            else if ((opt ==='=' && optOld===undefined) || (optOld==='=' && opt==='=')) return
+            else if (opt ==='='){
+                idx = disp.lastIndexOf(optOld);
+                num2 = disp.slice(idx+2);
+                disp = disp.concat(' = ', operate(Number(num1),Number(num2),optOld));
+                dispScreen.textContent = disp;
+                disp = "";
+                num1= undefined;
+                num2=undefined;
+                opt=undefined;
+            }
+            else if (optOld !== undefined){
+                idx = disp.lastIndexOf(optOld);
+                num2 = disp.slice(idx+2);
+                num1 = operate(Number(num1),Number(num2),optOld);
+                disp = disp.concat(' ',opt,' ');
+                dispScreen.textContent = disp;
+            }
+            else if (optOld === undefined){
+                num1 = disp;
+                disp = disp.concat(' ',opt,' ');
+                dispScreen.textContent = disp;
+            }
         }
-        else if (optOld !== undefined){
-            idx = disp.lastIndexOf(optOld);
-            num2 = disp.slice(idx+2);
-            num1 = operate(Number(num1),Number(num2),optOld);
-            disp = disp.concat(' ',opt,' ');
-            dispScreen.textContent = disp;
-        }
-        else if (optOld === undefined){
-            num1 = disp;
-            disp = disp.concat(' ',opt,' ');
-            dispScreen.textContent = disp;
-        }
-        
     })
 })
 
